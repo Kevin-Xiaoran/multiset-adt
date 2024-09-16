e5539d6e01001e6a84541ad51b91e16beb1138ca
Kairan Zhai <kairan95.zhai@gmail.com>
2024-09-16 12:21:02 -0400

    tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,10 @@
 public class Tree {
+
+    int _root;
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    public static void main(String[] args) {
+
+    }
+
+
 }
